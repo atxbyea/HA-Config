c2c944caa50c159befddfaac5c8bd8675dadb5f3
atxbyea <eirik.zakariassen@gmail.com>
2019-11-20 15:38:58 +0100

    Objective Nagging Shopowners Drink Alcoholic Gnomes

--- custom_components/old-pytradfri/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Implement an API wrapper around Ikea Tradfri."""
-
-from .error import (
-    PytradfriError, RequestError, ClientError, ServerError, RequestTimeout)
-from .gateway import Gateway
-
-__all__ = ['Gateway', 'PytradfriError', 'RequestError', 'ClientError',
-           'ServerError', 'RequestTimeout']
